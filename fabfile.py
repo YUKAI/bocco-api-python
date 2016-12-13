ef33a296473627ce88684d55ac6f582e9d249ef2
kiryuxxu <kiryu@ux-xu.com>
2016-12-13 15:36:43 +0900

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,4 +4,5 @@ from fabric.api import *
 @task
 def build_docs():
     with lcd('docs'):
+        local('rm -rf _build/html/*.html')
         local('source ../.virtualenv-sphinx/bin/activate && make html')
