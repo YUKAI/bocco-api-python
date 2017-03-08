fedd95bd5a1178484b2e7b1f0b27b82ada24e7a2
kiryuxxu <kiryu@ux-xu.com>
2017-03-08 15:34:03 +0900

    Version bump to 0.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='bocco',
-    version='0.1.2',
+    version='0.1.3',
     description='BOCCO API Client',
     long_description=long_description,
     url='https://github.com/YUKAI/bocco-api-python',
